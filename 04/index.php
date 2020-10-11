2de712c0cf81deee687be1aae03af32ea6e27c04
sbrocks777 <rajbharsushilkumar47@gmail.com>
2020-10-11 18:16:22 +0530

    ADD: Assignment No.06

--- 04/index.php
+++ 04/index.php
@@ -2,7 +2,9 @@
 <div class="container shadow p-3 mb-5 bg-white rounded">
   <div class="d-flex mt-5 justify-content-between align-items-center">
     <h2 class="">Employee Details</h2>
-    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="Create new employee">Add new employee</button>
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="Create new employee">
+      + new employee
+    </button>
     <!-- <button class="btn btn-primary float-right d-inline">Add new employee</button> -->
   </div>
   <hr>
@@ -12,7 +14,7 @@
   $sql = "SELECT * from employees";
   if ($result = mysqli_query($conn, $sql)) {
     echo
-      "<table class='table table-bordered table-striped table-sm'> 
+      "<table class='table'> 
         <thead>
           <tr>
             <th scope='col'>#</th>
